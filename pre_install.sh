c699f9a85381a0dbf9e11e473d4deaafd5e3edb4
root <root@jump.localdomain>
2021-02-02 20:21:36 +0100

    pre_install.sh

---
+++ pre_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+
+yum -y install docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
+
